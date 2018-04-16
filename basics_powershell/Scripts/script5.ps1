d6ec6b64513eb096255130514fea8dd0edb4d609
shaunak1105 <shaunak1105@gmail.com>
2018-04-16 11:20:19 +0530

    parsing html 2

--- basics_powershell/Scripts/script5.ps1
+++ basics_powershell/Scripts/script5.ps1
@@ -1,1 +1,9 @@
-﻿
+﻿$parseURL = 'https://www.reddit.com/r/PowerShell/'
+$webRequest = Invoke-WebRequest -Uri $parseURL
+
+$webRequest
+
+$webRequest | Get-Member
+
+$webRequest.ParsedHtml.getElementsByTagName('p') | Where-Object {$_.ClassName -eq 'title'}
+$webRequest.ParsedHtml.getElementsByTagName('p') | Where-Object {$_.ClassName -eq 'title'} | Select-Object -ExpandProperty OuterText
